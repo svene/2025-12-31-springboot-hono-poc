95638503cababb1154c3d3aa80fbe1dfd3ffe536
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2025-12-31 20:51:55 +0100

    connect springboot controller with Hono endpoint. Works :-)

--- hono/src/index.ts
+++ hono/src/index.ts
@@ -1,9 +1,18 @@
 import { Hono } from 'hono'
+import { html } from "hono/html"
 
 const app = new Hono()
 
 app.get('/', (c) => {
 	return c.text('Hello Hono!')
 })
+
+app.get('/hello', (c) => {
+	return c.html(
+		html`<!doctype html>
+      <h1>Hello from Hono!</h1>`
+	)
+})
+
 
 export default app
--- springboot/src/main/java/dev/svenehrke/springboothonopoc/UIController.java
+++ springboot/src/main/java/dev/svenehrke/springboothonopoc/UIController.java
@@ -1,26 +1,38 @@
 package dev.svenehrke.springboothonopoc;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestClient;
+
+import java.net.URI;
+import java.util.Map;
 
 @Controller
 public class UIController {
 
+	private final RestClient restClient;
+
+	public UIController() {
+		this.restClient = RestClient.builder().build();
+	}
+
 	@GetMapping("/")
 	@ResponseBody
 	public String home() {
-		return """
-            <!DOCTYPE html>
-            <html>
-              <head>
-                <title>Spring HTML Response</title>
-              </head>
-              <body>
-                <h1>Hello from Spring MVC!</h1>
-                <p>This HTML is returned directly from the controller.</p>
-              </body>
-            </html>
-            """;
+		// Step 1: Gather data from DB or services
+		// ...
+		// Step 2: Prepare HTTP POST request to HONO
+		String htmlResponse = restClient
+			.get()
+			.uri("http://localhost:3000/hello")
+			.header(HttpHeaders.ACCEPT, MediaType.TEXT_HTML_VALUE)
+			.retrieve()
+			.body(String.class);
+
+		return htmlResponse;
 	}
 }
