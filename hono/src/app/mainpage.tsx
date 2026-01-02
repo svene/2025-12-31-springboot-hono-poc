5b41700dac9cf058dd0e4d515df70b594de33b36
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2026-01-02 17:33:55 +0100

    removed shared ui and moved it to the POST controller

--- hono/src/app/mainpage.tsx
+++ hono/src/app/mainpage.tsx
@@ -1,14 +1,38 @@
 import {Hono} from "hono";
 import {Person} from "../ui/model";
-import {mainpage_ui} from "../ui/mainpage_ui";
+import {BulmaPage} from "../ui/components/bulmapage";
 
 export const URL = '/people';
+
+function ui(people: Person[]) {
+	return (
+		<BulmaPage>
+			<>
+				<h1 class="title">Application</h1>
+				<table class="table">
+					<thead>
+					<tr>
+						<th>Firstname</th>
+						<th>Lastname</th>
+					</tr>
+					</thead>
+					{people.map((it) => (
+						<tr>
+							<td>{it.firstName}</td>
+							<td>{it.lastName}</td>
+						</tr>
+					))}
+				</table>
+			</>
+		</BulmaPage>
+	);
+}
 
 function init(hono: Hono) {
 	hono.post(URL, async (c) => {
 		console.log('in POST');
 		const people = await c.req.json() as Person[];
-		return c.render(mainpage_ui.ui(people));
+		return c.render(ui(people));
 	});
 }
 
--- hono/src/dev/mainpage_dev.tsx
+++ hono/src/dev/mainpage_dev.tsx
@@ -1,12 +1,10 @@
 import {Hono} from "hono";
 import {Person} from "../ui/model";
-import {mainpage_ui} from "../ui/mainpage_ui";
 import {postForHtml} from "./dev_forwarder";
-
-export const URL = '/people';
+import {mainpage} from "../app/mainpage";
 
 function init(hono: Hono) {
-	hono.get(URL, async (c) => {
+	hono.get(mainpage.URL, async (c) => {
 		const people: Person[] = [
 			{firstName: 'John_dev', lastName: 'Lennon'},
 			{firstName: 'Paul_dev', lastName: 'McCartney'},
@@ -21,19 +19,6 @@ function init(hono: Hono) {
 	});
 }
 
-function init0(hono: Hono) {
-	hono.get(URL, (c) => {
-		const people: Person[] = [
-			{firstName: 'John', lastName: 'Lennon'},
-			{firstName: 'Paul', lastName: 'McCartney'},
-			{firstName: 'George', lastName: 'Harrison'},
-			{firstName: 'Ringo', lastName: 'Starr'},
-		];
-		return c.render(mainpage_ui.ui(people));
-	});
-}
-
 export const mainpage_dev = {
-	URL,
 	init,
 }
