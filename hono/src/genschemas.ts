6c5118115941ce59af7ff16b8f41d95be9566384
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2026-01-03 17:14:12 +0100

    Revert "playing around with toJSONSchema() for openapi interface"
    This reverts commit f4ee7cbcbb7f2f886c1522bea8169fe1c9c83929.

--- hono/src/genschemas.ts
+++
@@ -1,9 +0,0 @@
-import * as z from "zod";
-import {PeopleSchema} from "./ui/model";
-
-const x = z.toJSONSchema(PeopleSchema, {
-	//target: "openapi-3.0",
-	reused: "ref"
-});
-
-console.log(JSON.stringify(x, null, 2));
