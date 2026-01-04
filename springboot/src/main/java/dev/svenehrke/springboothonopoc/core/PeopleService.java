9f6a86d2f4585ee6fb9320f6ef10b77e34e22c26
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2026-01-04 10:10:24 +0100

    explicit DB->VM mapping

--- springboot/src/main/java/dev/svenehrke/springboothonopoc/core/PeopleService.java
+++ springboot/src/main/java/dev/svenehrke/springboothonopoc/core/PeopleService.java
@@ -1,8 +1,10 @@
 package dev.svenehrke.springboothonopoc.core;
 
+import org.jspecify.annotations.NonNull;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.function.Function;
 
 @Service
 public class PeopleService {
@@ -14,9 +16,13 @@ public class PeopleService {
 
 	public List<PersonTableRowVM> people() {
         return peopleRepository.people().stream()
-            .map(it -> new PersonTableRowVM(it.name().firstName(), it.name().lastName(), it.address().streetName()))
+            .map(mapDBtoVM())
             .toList();
 
     }
+
+	private static @NonNull Function<DBResult, PersonTableRowVM> mapDBtoVM() {
+		return it -> new PersonTableRowVM(it.name().firstName(), it.name().lastName(), it.address().streetName());
+	}
 
 }
