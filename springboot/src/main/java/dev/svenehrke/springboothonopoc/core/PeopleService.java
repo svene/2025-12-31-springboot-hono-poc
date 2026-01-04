8b8a83aa81389d5a864b334c2d7acf10a6ebbd9d
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2026-01-04 08:32:20 +0100

    spring: using datafaker to generate Person data

--- springboot/src/main/java/dev/svenehrke/springboothonopoc/core/PeopleService.java
+++ springboot/src/main/java/dev/svenehrke/springboothonopoc/core/PeopleService.java
@@ -1,17 +1,22 @@
 package dev.svenehrke.springboothonopoc.core;
 
+import net.datafaker.Faker;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Stream;
 
 @Service
 public class PeopleService {
     public List<Person> people() {
-        return List.of(
-            new Person("John", "Lennon"),
-            new Person("Paul", "McCartney"),
-            new Person("George", "Harrison"),
-            new Person("Ringo", "Starr")
-        );
+        Faker faker = new Faker(new Random(0));
+        return Stream.generate(() -> {
+                var name = faker.name();
+                return new Person(name.firstName(), name.lastName());
+			})
+            .limit(20)
+            .toList();
+
     }
 }
